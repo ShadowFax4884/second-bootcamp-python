d07c662303d08e804ec2b2027f9fe0a3321e8a6f
Mohsen Yousefzadeh <nash.gjohn1992@gmail.com>
2019-01-09 01:59:43 +0330

    tutorial 1 _ page 4

---
+++ 001-tutorial01/page4.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Mohsen Yousefzadeh
+#
+# Created:     09/01/2019
+# Copyright:   (c) mohsen yousefzadeh 2019
+# Licence:     ArcGIS 10.2.2
+#-------------------------------------------------------------------------------
+myName = "Patrick"
+yourname = 'holly'
+print ' my name is '+ myName + ' and your name is ' + yourname + ' .'
+streetName = 'Candy Lane'
+addressNumber = 313
+percentOccupied = 32.34
+ownerName = 'brown, Charles'
+print 'The owner of ' + str(addressNumber)[0:1] + ' ' + streetName + 'is' + ownerName + ' .'
+print 'The occupied area is ' + str(100 - percentOccupied) + ' .'
+print 'The street number is ' + streetName[0:5] + ' .'
+
+
+
+
+
+
+
+
+
