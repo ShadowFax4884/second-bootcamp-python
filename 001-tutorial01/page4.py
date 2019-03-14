2c6ccec0786e7ef0e21f29d84a88422143deea4d
Mohsen Yousefzadeh <nash.gjohn1992@gmail.com>
2019-03-14 10:48:22 +0330

    end of tutorial 1-1

--- 001-tutorial01/page4.py
+++ 001-tutorial01/page4.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Name:        module1
 # Purpose:
 #
@@ -7,23 +7,19 @@
 # Created:     09/01/2019
 # Copyright:   (c) mohsen yousefzadeh 2019
 # Licence:     ArcGIS 10.2.2
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 myName = "Patrick"
-yourname = 'holly'
-print ' my name is '+ myName + ' and your name is ' + yourname + ' .'
+yourName = 'holly'
+print 'My name is '+ myName + ' and your name is ' + yourName + '.'
 streetName = 'Candy Lane'
 addressNumber = 313
 percentOccupied = 32.34
 ownerName = 'brown, Charles'
-print 'The owner of ' + str(addressNumber)[0:1] + ' ' + streetName + 'is' + ownerName + ' .'
-print 'The occupied area is ' + str(100 - percentOccupied) + ' .'
-print 'The street number is ' + streetName[0:5] + ' .'
-
-
-
-
-
-
-
-
-
+print 'The owner of ' + str(addressNumber)[0:1] + ' ' + streetName + 'is' + ownerName + '.'
+print 'The occupied area is ' + str(100 - percentOccupied) + '.'
+print 'The street number is ' + streetName[0:5] + '.'
+print 'The street number is ' + streetName[-4:] + '.'
+listNames = ['David', 'Cindy', 'Mohsen', 'Patrick', 'Ali']
+print listNames[2]
+print listNames[4]
+print listNames[1:5]
